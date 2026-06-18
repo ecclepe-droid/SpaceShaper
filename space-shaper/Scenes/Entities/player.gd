0f2747fb9ede6e9fef87b07fda13415c2ea4c298
ecclepe-droid <ecclepe@whs.school.nz>
2026-06-18 12:51:01 +1200

    Add files via upload
    version control!

---
+++ space-shaper/Scenes/Entities/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var move_speed = 50
+
+
+# Called every frame. 'delta' is the elapsed time
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var move_speed = 2.5
+	position += (direction * move_speed)
+	var rotate = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	rotation = 1
